7fb939f9b781a96de97a94d513d957f6a0f92aa2
leis-coder <63501096+leis-coder@users.noreply.github.com>
2020-04-11 15:29:46 +0300

    Add files via upload

---
+++ 5_lab/Ex3/Ex3/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        public static int Summ(int[] a)
+        {
+            int s = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                s += a[i];
+            }
+            return s;
+        }
+
+        public static float Average(int[] a)
+        {
+            return Summ(a) / a.Length;
+        }
+
+        public static int Positive(int[] a)
+        {
+            int P = 0;
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] > 0)
+                    P += a[i];
+            return P;
+        }
+
+        public static int Even(int[] a)
+        {
+            int E = 0;
+            for (int i = 0; i < a.Length; i += 2)
+                E += a[i];
+            return E;
+        }
+
+        public static int Maximum(int[]a)
+        {
+            int m = a[0];
+            int mi = 0;
+            for (int i=1; i<a.Length; i++)
+            {
+                if (m<a[i])
+                {
+                    m = a[i];
+                    mi = i;
+                }
+            }
+            return mi;
+        }
+
+        public static int Minimum(int[] a)
+        {
+            int m = a[0];
+            int mi = 0;
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (m > a[i])
+                {
+                    m = a[i];
+                    mi = i;
+                }
+            }
+            return mi;
+        }
+
+        public static int Proisvedenie(int[]a)
+        {
+            int p = 1;
+            int c, b;
+
+            if (Minimum(a) < Maximum(a))
+            {
+                c = Maximum(a);
+                b = Minimum(a);
+            }
+            else
+            {
+                c = Maximum(a);
+                b = Minimum(a);
+            }
+            for (int i =c+1; i<b; i++)
+            {
+                p = p * a[i];
+            }
+            return p;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Summ is: {0}", Summ(a));
+            Console.WriteLine("Average result is: {0}", Average(a));
+            Console.WriteLine("Summ of positive elements is: {0}", Positive(a));
+            Console.WriteLine("Summ of elements with even index is: {0}", Even(a));
+
+            Console.WriteLine("Maximum is: {0}", a[Maximum(a)]);
+            Console.WriteLine("Minimum is: {0}", a[Minimum(a)]);
+            Console.WriteLine(Proisvedenie(a));
+        }
+    }
+}
---
+++ 5_lab/Loop/Loop/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] MyArray;
+            int n = int.Parse(Console.ReadLine());
+            MyArray = new int[n];
+            for (int i = 0; i < MyArray.Length; ++i) 
+            {
+                Console.Write("a[{0}]=", i); 
+                MyArray[i] = int.Parse(Console.ReadLine()); 
+            }
+            foreach (int x in MyArray) Console.Write("{0} ", x);
+
+            //int[] myArray = { 100, 1, 32, 3, 14, 25, 6, 17, 8, 99 };
+
+            //for (int i=0; i<10; i++)
+            //{
+            //    if (myArray[i] % 2 == 0)
+            //        myArray[i] = 0;
+
+            //    Console.Write("{0}  ", myArray[i]);
+            // }
+
+
+
+            //    Console.Write("n = ");
+            //    int n = int.Parse(Console.ReadLine());
+
+            //    // while:
+            //    Console.Write("\nwhile: \t\t");
+            //    int i = 1;
+            //    while (i <= n)
+            //    {
+            //        Console.Write(" " + i);
+            //        i += 2;
+            //    }
+
+            //    // do while:
+            //    Console.Write("\ndo while: \t");
+            //    i = 1;
+            //    do
+            //    {
+            //        Console.Write(" " + i);
+            //        i += 2;
+            //    }
+            //    while (i <= n);
+
+            //    // for: 
+            //    Console.Write("\nFor: \t\t");
+            //    for (i = 1; i <= n; i += 2)
+            //    {
+            //        Console.Write(" " + i);
+            //    }
+
+
+
+            //    double x, x1, x2, y;
+            //    Console.Write("Введите границы интервала. х1 = ");
+            //    x1 = double.Parse(Console.ReadLine());
+            //    Console.Write("\nх2 = ");
+            //    x2 = double.Parse(Console.ReadLine());
+
+            //    x = x1;
+            //    do
+            //    {
+            //        y = Math.Sin(x);
+            //        Console.WriteLine("Sin({0}) = {1}", x, y);
+            //        x = x + 0.01;
+            //    }
+            //    while (x <= x2);
+
+
+
+            //    int a, b, temp;
+
+            //    Console.Write("Введите значение первого числа. а = ");
+            //    a = int.Parse(Console.ReadLine());
+            //    Console.Write("Введите значение второго числа. b = ");
+            //    b = int.Parse(Console.ReadLine());
+
+            //    temp = a;
+
+            //    while (temp != b)
+            //    {
+            //        a = temp;
+            //        if (a < b)
+            //        {
+            //            temp = a;
+            //            a = b;
+            //            b = temp;
+            //        }
+            //        temp = a - b;
+            //    }
+
+            //    Console.WriteLine("Наибольший общий делитель, найденный с помощью цикла с предусловием: {0}", temp);
+        }
+    }
+}
